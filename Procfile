789fc2387699f989910f3e7416797fed5ea1576b
brin2806 <brindha@example.com>
2015-09-09 17:43:35 +1000

    added procfile

---
+++ Procfile
@@ -0,0 +1,2 @@
+# Procfile
+web: gunicorn my_project.wsgi --log-file -
--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,14 @@
+
+# app1/views.py
 from django.shortcuts import render
 
-# Create your views here.
+from libs.open_weather_map import OpenWeatherMap
+
+
+def index(request):
+    open_weather_map = OpenWeatherMap()
+
+    context = {
+        'forecast': open_weather_map.get_forecast()
+        }  # So far, we are not specifying any custom context
+    return render(request, 'app1/index.html', context)
