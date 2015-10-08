6d8bc864ac2759e2fc732b3b6e7662f2d3314ee8
brin2806 <brindha@example.com>
2015-10-08 20:05:51 +1100

    minor changes to wrapper.py

--- app1/views.py
+++ app1/views.py
@@ -2,13 +2,26 @@
 # app1/views.py
 from django.shortcuts import render
 
-from libs.open_weather_map import OpenWeatherMap
+from libs.open_weather_map.wrapper import OpenWeatherMap
+from app1 import forms as app1_forms
 
 
 def index(request):
-    open_weather_map = OpenWeatherMap()
-
-    context = {
-        'forecast': open_weather_map.get_forecast()
-        }  # So far, we are not specifying any custom context
-    return render(request, 'app1/index.html', context)
+    context = {}
+    # If request has GET parameters, this means it was created when the user clicked the submit
+    # submit button on the form
+    if request.GET:
+         # Create a bound form based on the GET parameters to the request
+        form = app1_forms.LocationForm(request.GET)
+        if form.is_valid():
+            # If the form parameters are valid, search for weather based on the form data
+            # If the form parameters are invalid, the page will be loaded showing the errors
+            open_weather_map = OpenWeatherMap()
+            context['forecast'] = open_weather_map.get_forecast(
+                    city=form.cleaned_data['city'], country=form.cleaned_data['country'])
+    else:
+        # This is an initial page load, so create an unbound form
+        form = app1_forms.LocationForm()
+    # Add the form to the page context so it is available to the template
+    context['form'] = form
+    return render(request, 'app1/index.html', context)    
---
+++ libs/open_weather_map/wrapper.py
@@ -0,0 +1,34 @@
+# libs/open_weather_map/wrapper.py
+import requests
+
+
+class OpenWeatherMap():
+
+    def __init__(self):
+        # This is the base URL for the API and will be common to all requests
+        self.api_url = 'http://api.openweathermap.org/data/2.5/'
+
+    def get_forecast(self, city='', country=''):
+    # Append the "forecast" path to the base API URL
+        url = '%sforecast' % self.api_url
+
+        # URL parameters
+        params = {}
+        if city and country:
+            params['q'] = '%s,%s' % (city, country)
+        else:
+            params['lat'] = -33.8650
+            params['lon'] = 151.2094
+
+
+        # Do a get request on the forecast url, with the lat/lon params
+        response = requests.get(url, params=params)
+
+        # It's always a good idea to initialize results to empty, and update if data received
+        results = {}
+        # If the request was successful
+        if response.status_code == requests.codes.ok:
+            # Get the response body in JSON format
+            results = response.json()
+
+        return results        
