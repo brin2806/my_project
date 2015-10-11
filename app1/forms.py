e71447ebf6373dc41a1852349db2c2c43b74e05c
brin2806 <brindha@example.com>
2015-10-11 17:03:36 +1100

    removed duplicate file from lib directory

---
+++ app1/forms.py
@@ -0,0 +1,9 @@
+# app1/forms.py
+from django import forms
+
+
+class LocationForm(forms.Form):
+    city = forms.CharField(label='City', max_length=50)
+    country = forms.CharField(label='Country', max_length=2)
+
+    
